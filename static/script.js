b48c91477e685ab8315fe8b77067142ee15ba6a2
Abhishek Bhoyar <abhibhoyar141@gmail.com>
2024-01-14 20:15:06 +0530

    donee

--- static/script.js
+++ static/script.js
@@ -20,35 +20,20 @@ window.addEventListener('load', function () {
 })
 
 const text1_options = [
-  "1st Event heading",
-  "2nd event heading",
-  "3rd event heading",
-  "4th event heading",
-  "5th event heading"
+  "Inauguration Event",
+  
 
 ];
 const text2_options = [
-  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero fugit ratione nulla vitae iste eius consectetur dolor doloremque repellat nihil.",
-  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero fugit ratione nulla vitae iste eius consectetur dolor doloremque repellat nihil.",
-  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero fugit ratione nulla vitae iste eius consectetur dolor doloremque repellat nihil.",
-  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero fugit ratione nulla vitae iste eius consectetur dolor doloremque repellat nihil.",
-  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero fugit ratione nulla vitae iste eius consectetur dolor doloremque repellat nihil."
+  "YCCE's GFG Student Chapter."
 ];
 
 const text3_options = [
-  "1111111",
-  "2222222",
-  "3333333",
-  "4444444",
-  "5555555"
+    "7th Nov 2023"
 ];
 const color_options = ["#f5f1ed"];
 const image_options = [
-  "https://wallpapers.com/images/thumbnail/spiderman-metal-suit-painting-ccnif799yrhq1avn.webp",
-  "https://wallpapers.com/images/thumbnail/thor-battle-armor-hduiuwcgb91saehk.webp",
-  "https://wallpapers.com/images/thumbnail/captain-america-android-b72spfe5ymkzw3c5.webp",
-  "https://wallpapers.com/images/high/ironman-hd-kj6g64imjfxtenwx.webp",
-  "https://wallpapers.com/images/high/henry-cavill-as-superman-zsg0t2reyput4g60.webp"
+  "/static/images/Events/inaug.jpeg",
 ];
 var i = 0;
 const currentOptionText1 = document.getElementById("current-option-text1");
